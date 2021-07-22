100faeea88dbd608035005543df97bbc6a5e764a
Oelin <me.oelin@gmail.com>
2021-07-22 11:38:56 +0100

    included related subjects in achivement recommendation input

--- src/pages/Achievements/Rec.js
+++ src/pages/Achievements/Rec.js
@@ -8,28 +8,6 @@ function getCourseRecommendations(about) {
 }
 
 
-// async function mostCommonSubject(achievements) {
-//   let tab = {}
-//   let list = []
-//
-//   achievements.forEach(({ Associations }) => list.push(...Associations));
-//   list.forEach(subject => tab[subject] = ~~tab[subject] + 1)
-//
-//   return Object.entries(tab).map(([k, v]) => `${v}: ${k}`).sort().slice(-1)[0].split(': ')[1]
-// }
-//
-//
-// async function mostLikelySubject(achievements) {
-//   const combinedText = achievements.map(({ About, Name }) => `${About} ${Name}`).join(' ')
-//   console.log(combinedText)
-//
-//   const subjectPercentages = await API.get(`/topics/${encodeURIComponent(combinedText)}`)
-//   console.log(subjectPercentages)
-//
-//   return 'maths'
-// }
-//
-
 const CourseList = styled.ul`
   li {
     text-transform: Capitalize;
@@ -45,7 +23,7 @@ export default ({ achievements  }) => {
   useEffect(() => {
     (!courses || (achievements != previous)) && (async () => {
 
-      let about = achievements.map(({ About }) => About).join(' ')
+      let about = achievements.map(({ About, Associations }) => `${About} ${Associations.join(' ')}`).join(' ')
       let courses = await getCourseRecommendations(about)
 
       setCourses(courses)
